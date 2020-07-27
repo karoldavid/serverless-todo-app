9bc749285695beeda0b4a24c4a49723478b61f7a
karoldavid <k.zyskowski@live.com>
2020-07-27 20:47:22 +0200

    chore: connfigure auth0 login via environment variables

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,8 +3,7 @@ const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  domain: process.env.REACT_APP_AUTH0_CONFIG_DOMAIN,
+  clientId: process.env.REACT_APP_AUTH0_CONFIG_CLIENT_ID,
+  callbackUrl: process.env.REACT_APP_AUTH0_CONFIG_CALLBACK_URL
 }
