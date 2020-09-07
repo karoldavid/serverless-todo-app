c7cc52dcb70379b78baa1565effff0c044208576
karoldavid <k.zyskowski@live.com>
2020-09-07 20:51:24 +0200

    feat: add cors headers for http lambda generateUploadUrl

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -50,6 +50,10 @@ export const handler: APIGatewayProxyHandler = async (
 
   return {
     statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
     body: JSON.stringify({
       newItem: newItem,
       uploadUrl: url
